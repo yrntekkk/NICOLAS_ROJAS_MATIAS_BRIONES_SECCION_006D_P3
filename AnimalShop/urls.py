2fb7de330c845c963bee87c1fd4c7b06ac9eae70
Matias Ignacio Briones Alegria <mati.brionesa@duocuc.cl>
2024-06-26 17:13:44 -0400

    Agregamos Templates y Css
    Agregamos templates del login y registro y el css

--- AnimalShop/urls.py
+++ AnimalShop/urls.py
@@ -21,4 +21,6 @@ from tienda import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('registro/', views.registro, name='registro')
 ]
